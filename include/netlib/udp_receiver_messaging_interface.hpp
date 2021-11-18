6408db7834ac6c9b43e71c583494a547a2c28ee9
axilmar <axilmar@gmail.com>
2021-11-18 14:08:49 +0200

    Added specific udp receiver sending messaging interface.

---
+++ include/netlib/udp_receiver_messaging_interface.hpp
@@ -0,0 +1,39 @@
+#ifndef NETLIB_UDP_RECEIVER_MESSAGING_INTERFACE_HPP
+#define NETLIB_UDP_RECEIVER_MESSAGING_INTERFACE_HPP
+
+
+#include "udp_messaging_interface.hpp"
+
+
+namespace netlib {
+
+
+    /**
+     * A sending messaging interface for a specific receiver.
+     */
+    class udp_receiver_messaging_interface : public sending_messaging_interface {
+    public:
+        /**
+         * Constructor.
+         * @param addr the receiver's address.
+         * @param messaging_interface the udp sending interface to use for sending a message.
+         */
+        udp_receiver_messaging_interface(socket_address&& addr, std::shared_ptr<udp_messaging_interface>&& messaging_interface);
+
+        /**
+         * Sends a message to a specific address.
+         * @param msg message to send.
+         * @return true if the message was sent, false if it could not be sent.
+         */
+        bool send_message(const message& msg) override;
+
+    private:
+        socket_address m_receiver_address;
+        std::shared_ptr<udp_messaging_interface> m_messaging_interface;
+    };
+
+
+} //namespace netlib
+
+
+#endif //NETLIB_UDP_RECEIVER_MESSAGING_INTERFACE_HPP
---
+++ src/netlib/udp_receiver_messaging_interface.cpp
@@ -0,0 +1,22 @@
+#include "netlib/udp_receiver_messaging_interface.hpp"
+
+
+namespace netlib {
+
+
+    //Constructor.
+    udp_receiver_messaging_interface::udp_receiver_messaging_interface(socket_address&& addr, std::shared_ptr<udp_messaging_interface>&& messaging_interface)
+        : m_receiver_address(std::move(addr))
+        , m_messaging_interface(std::move(messaging_interface))
+    {
+    }
+
+
+    //Sends a message to a specific address.
+    bool udp_receiver_messaging_interface::send_message(const message& msg) {
+        m_messaging_interface->set_receiver_address(m_receiver_address);
+        return m_messaging_interface->send_message(msg);
+    }
+
+
+} //namespace netlib
