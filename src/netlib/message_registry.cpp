3089600078384292b9dbcc894612f3818a01512a
axilmar <axilmar@gmail.com>
2021-11-22 16:13:56 +0200

    Fixed checking for invalid message id.

--- src/netlib/message_registry.cpp
+++ src/netlib/message_registry.cpp
@@ -1,6 +1,7 @@
 #include <mutex>
 #include <vector>
 #include "netlib/message_registry.hpp"
+#include "netlib/stringstream.hpp"
 #include "internals/message_id.hpp"
 
 
@@ -69,32 +70,52 @@ namespace netlib {
         //get the internal id
         const netlib::internals::message_id& internal_id = reinterpret_cast<const netlib::internals::message_id&>(id);
 
-        //try to find the message in the current thread's table
+        //if the message exists in the current thread's namespace table
         if (internal_id.parts.namespace_index < message_creation_functions.size()) {
             const message_creation_function_table& msg_table = message_creation_functions[internal_id.parts.namespace_index];
+
+            //if the message exists in the current thread's message table
             if (internal_id.parts.message_index < msg_table.size() && msg_table[internal_id.parts.message_index]) {
                 return msg_table[internal_id.parts.message_index](memres);
             }
         }
 
-        //not found in the current thread's table; make room in the thread's table
-        if (internal_id.parts.namespace_index >= message_creation_functions.size()) {
-            message_creation_functions.resize(internal_id.parts.namespace_index + 1);
-        }
-        message_creation_function_table& msg_table = message_creation_functions[internal_id.parts.namespace_index];
-        if (internal_id.parts.message_index >= msg_table.size()) {
-            msg_table.resize(internal_id.parts.message_index + 1, nullptr);
-        }
-
         //copy the function from the global table to the local table
         {
+            //get global data and lock them
             message_registry_data& mrd = get_message_registry_data();
             std::lock_guard lock(mrd.mutex);
+
+            //check if the namespace exists in the global namespace table
+            if (internal_id.parts.namespace_index >= mrd.message_creation_functions.size()) {
+                throw message_error(stringstream() << "invalid message id: " << id);
+            }
+
+            const auto& global_msg_table = mrd.message_creation_functions[internal_id.parts.namespace_index];
+
+            //check if the message exists in the global message table
+            if (internal_id.parts.message_index >= global_msg_table.size() || !global_msg_table[internal_id.parts.message_index]) {
+                throw message_error(stringstream() << "invalid message id: " << id);
+            }
+
+            //make room in the thread namespace table
+            if (internal_id.parts.namespace_index >= message_creation_functions.size()) {
+                message_creation_functions.resize(internal_id.parts.namespace_index + 1);
+            }
+
+            message_creation_function_table& msg_table = message_creation_functions[internal_id.parts.namespace_index];
+
+            //make room in the thread message table
+            if (internal_id.parts.message_index >= msg_table.size()) {
+                msg_table.resize(internal_id.parts.message_index + 1, nullptr);
+            }
+
+            //copy the function
             msg_table[internal_id.parts.message_index] = mrd.message_creation_functions[internal_id.parts.namespace_index][internal_id.parts.message_index];
         }
 
         //create the message
-        return msg_table[internal_id.parts.message_index](memres);
+        return message_creation_functions[internal_id.parts.namespace_index][internal_id.parts.message_index](memres);
     }
 
 
