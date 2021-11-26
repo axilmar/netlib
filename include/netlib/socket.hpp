1ffc5d34105ddc722f0c4fe2114e55a83731a91f
axilmar <axilmar@gmail.com>
2021-11-26 18:20:46 +0200

    Added the socket class.

---
+++ include/netlib/socket.hpp
@@ -0,0 +1,210 @@
+#ifndef NETLIB_SOCKET_HPP
+#define NETLIB_SOCKET_HPP
+
+
+#include <utility>
+#include "address_family.hpp"
+#include "socket_type.hpp"
+#include "protocol.hpp"
+#include "socket_address.hpp"
+
+
+namespace netlib {
+
+
+    /**
+     * A socket.
+     */
+    class socket {
+    public:
+        /**
+         * The default constructor.
+         * The socket is initialized in an invalid state.
+         */
+        socket();
+
+        /**
+         * Constructor from address family, socket type and protocol type.
+         * @param af address family.
+         * @param type socket type.
+         * @param protocol protocol.
+         * @exception std::invalid_argument thrown if the socket cannot be created.
+         */
+        socket(int af, int type, int protocol);
+
+        /**
+         * Constructor from address family, socket type and protocol type.
+         * @param af address family.
+         * @param st socket type.
+         * @param p protocol.
+         * @exception std::invalid_argument thrown if the socket cannot be created.
+         */
+        socket(address_family af, socket_type st, protocol p);
+
+        /**
+         * Constructor from address family and socket type.
+         * The protocol is autodetected based on socket type.
+         * @param af address family.
+         * @param st socket type.
+         * @exception std::invalid_argument thrown if the socket cannot be created.
+         */
+        socket(address_family af, socket_type st);
+
+        /**
+         * The class is not copyable.
+         */
+        socket(const socket&) = delete;
+
+        /**
+         * The move constructor.
+         * @param src socket; on return, it becomes invalid.
+         */
+        socket(socket&& src);
+
+        /**
+         * Shuts down and closes the socket.
+         */
+        ~socket();
+
+        /**
+         * The class is not copyable.
+         */
+        socket& operator = (const socket&) = delete;
+
+        /**
+         * The move assignment operator.
+         * @param src source; on return, it becomes invalid.
+         * @return reference to this.
+         */
+        socket& operator = (socket&& src);
+
+        /**
+         * Puts the socket in listening state.
+         * @param max_connections maximum number of connections; if 0, then the system will select max number of connections.
+         * @exception std::runtime_error thrown if there was an error.
+         */
+        void listen(int max_connections = 0);
+
+        /**
+         * Accepts a connection.
+         * @return pair of created socket and socket address of the connecting entity.
+         * @exception std::runtime_error thrown if there was an error.
+         */
+        std::pair<socket, socket_address> accept();
+
+        /**
+         * Binds the socket to the specific address.
+         * @param address address.
+         * @exception std::runtime_error thrown if there was an error.
+         */
+        void bind(const socket_address& address);
+
+        /**
+         * Connects the socket to the specific address.
+         * @param address address.
+         * @exception std::runtime_error thrown if there was an error.
+         */
+        void connect(const socket_address& address);
+
+        /**
+         * Sends data over the network.
+         * @param buffer pointer to data to send.
+         * @param size number of bytes to send.
+         * @param flags send flags.
+         * @return number of bytes sent, 0 if the connection is closed.
+         * @exception std::runtime_error thrown if there was an error.
+         */
+        size_t send(const void* buffer, size_t size, int flags = 0);
+
+        /**
+         * Sends data over the network to a specific address.
+         * @param buffer pointer to data to send.
+         * @param size number of bytes to send.
+         * @param address destination address.
+         * @param flags send flags.
+         * @return number of bytes sent, 0 if the connection is closed.
+         * @exception std::runtime_error thrown if there was an error.
+         */
+        size_t send(const void* buffer, size_t size, const socket_address& address, int flags = 0);
+
+        /**
+         * Receives data from the network.
+         * Blocking call.
+         * @param buffer pointer to buffer to store the received data.
+         * @param size number of bytes that can be stored in the buffer.
+         * @param flags receive flags.
+         * @return number of received bytes or 0 if the connection is closed.
+         * @exception std::runtime_error thrown if there was an error.
+         */
+        size_t receive(void* buffer, size_t size, int flags = 0);
+
+        /**
+         * Receives data from the network, along with the address of the sender.
+         * Blocking call.
+         * @param buffer pointer to buffer to store the received data.
+         * @param size number of bytes that can be stored in the buffer.
+         * @param address the address of the sender.
+         * @param flags receive flags.
+         * @return number of received bytes or 0 if the connection is closed.
+         * @exception std::runtime_error thrown if there was an error.
+         */
+        size_t receive(void* buffer, size_t size, socket_address& address, int flags = 0);
+
+        /**
+         * Sends a packet.
+         * @param buffer data to send.
+         * @param size number of bytes to send.
+         * @param flags send flags.
+         * @return number of bytes sent or 0 if the connection is closed.
+         * @exception std::runtime_error thrown if there was an error.
+         * @exception std::invalid_argument thrown if the given size exceeds the 32-bit limit.
+         */
+        size_t send_packet(const void* buffer, size_t size, int flags = 0);
+
+        /**
+         * Sends a packet to a specific address.
+         * @param buffer data to send.
+         * @param size number of bytes to send.
+         * @param address destination address.
+         * @param flags send flags.
+         * @return number of bytes sent, i.e. given size, or 0 if the connection is closed.
+         * @exception std::runtime_error thrown if there was an error.
+         * @exception std::invalid_argument thrown if the given size exceeds the 32-bit limit.
+         */
+        size_t send_packet(const void* buffer, size_t size, const socket_address& address, int flags = 0);
+
+        /**
+         * Receives a packet.
+         * @param buffer pointer to buffer to store the received data.
+         * @param size number of bytes to receive.
+         * @param flags receive flags.
+         * @return number of bytes received, i.e. given size, or 0 if the connection is closed.
+         * @exception std::runtime_error thrown if there was an error.
+         * @exception std::invalid_argument thrown if the given buffer is too small for the received data.
+         */
+        size_t receive_packet(void* buffer, size_t size, int flags = 0);
+
+        /**
+         * Receives a packet, returning the sender's address.
+         * @param buffer pointer to buffer to store the received data.
+         * @param size number of bytes to receive.
+         * @param address the address of the sender.
+         * @param flags receive flags.
+         * @return number of bytes received, i.e. given size, or 0 if the connection is closed.
+         * @exception std::runtime_error thrown if there was an error.
+         * @exception std::invalid_argument thrown if the given buffer is too small for the received data.
+         */
+        size_t receive_packet(void* buffer, size_t size, socket_address& address, int flags = 0);
+
+    private:
+        uintptr_t m_handle;
+
+        //internal constructor
+        socket(uintptr_t handle) : m_handle(handle) {}
+    };
+
+
+} //namespace netlib
+
+
+#endif //NETLIB_SOCKET_HPP
---
+++ src/netlib/socket.cpp
@@ -0,0 +1,304 @@
+#include "netlib/platform.hpp"
+#include <stdexcept>
+#include <limits>
+#include "netlib/socket.hpp"
+
+
+namespace netlib {
+
+
+    #ifdef _WIN32
+    static constexpr uintptr_t invalid_socket_handle = INVALID_SOCKET;
+    #endif
+
+
+    #ifdef linux
+    static constexpr uintptr_t invalid_socket_handle = -1;
+    #endif
+
+
+    //get protocol for socket type
+    static protocol protocol_from_socket_type(socket_type st) {
+        switch (st) {
+        case socket_type::stream:
+            return protocol::tcp;
+
+        case socket_type::datagram:
+            return protocol::udp;
+        }
+
+        throw std::logic_error("Unsupported socket type.");
+    }
+
+
+    //checks value stored in size_t against int
+    static void check_buffer_size(size_t size) {
+        if constexpr (sizeof(int) < sizeof(size_t)) {
+            if (size > std::numeric_limits<int>::max()) {
+                throw std::invalid_argument("Given size value too large for the sockets API.");
+            }
+        }
+    }
+
+
+    //handle send result
+    static size_t handle_send_result(uintptr_t &handle, size_t bytes_sent) {
+        if (bytes_sent >= 0) {
+            return bytes_sent;
+        }
+
+        #ifdef _WIN32
+        switch (WSAGetLastError()) {
+        case WSAESHUTDOWN:
+        case WSAECONNABORTED:
+        case WSAECONNRESET:
+            handle = invalid_socket_handle;
+            return 0;
+        }
+        #endif
+
+        #ifdef linux
+        switch (errno) {
+        case ECONNRESET:
+        case EPIPE:
+            handle = invalid_socket_handle;
+            return 0;
+
+        default:
+        }
+        #endif
+
+        throw std::runtime_error(get_last_error());
+    }
+
+
+    //handle receive result
+    static size_t handle_receive_result(uintptr_t& handle, size_t bytes_received) {
+        if (bytes_received > 0) {
+            return bytes_received;
+        }
+
+        if (bytes_received == 0) {
+            handle = invalid_socket_handle;
+            return 0;
+        }
+
+        throw std::runtime_error(get_last_error());
+    }
+
+
+    //invalid socket constructor.
+    socket::socket() 
+        : m_handle{ invalid_socket_handle }
+    {
+    }
+
+
+    //open socket
+    socket::socket(int af, int type, int protocol)
+        : m_handle{ ::socket(af, type, protocol) }
+    {
+        if (m_handle == invalid_socket_handle) {
+            throw std::runtime_error(get_last_error());
+        }
+    }
+
+
+    //open socket from enumerated values
+    socket::socket(address_family af, socket_type st, protocol p)
+        : socket(address_family_to_system_value(af), socket_type_to_system_value(st), protocol_to_system_value(p))
+    {
+    }
+
+
+    //detect protocol from socket type
+    socket::socket(address_family af, socket_type st)
+        : socket(af, st, protocol_from_socket_type(st))
+    {
+    }
+
+
+    //move constructor
+    socket::socket(socket&& src)
+        : m_handle(src.m_handle)
+    {
+        src.m_handle = invalid_socket_handle;
+    }
+
+
+    //close the socket
+    socket::~socket() {
+        if (m_handle != invalid_socket_handle) {
+            shutdown(m_handle, SD_BOTH);
+            closesocket(m_handle);
+        }
+    }
+
+
+    //move assignment
+    socket& socket::operator = (socket&& src) {
+        uintptr_t temp = m_handle;
+        src.m_handle = invalid_socket_handle;
+        m_handle = temp;
+        return *this;
+    }
+
+
+    //listen
+    void socket::listen(int max_connections) {
+        int error = ::listen(m_handle, max_connections = 0 ? SOMAXCONN : max_connections);
+
+        if (!error) {
+            return;
+        }
+
+        throw std::runtime_error(get_last_error());
+    }
+
+
+    //accept
+    std::pair<socket, socket_address> socket::accept() {
+        socket_address addr;
+        auto new_socket_handle = ::accept(m_handle, reinterpret_cast<sockaddr*>(addr.data()), nullptr);
+
+        if (new_socket_handle != invalid_socket_handle) {
+            return { socket(new_socket_handle), addr };
+        }
+
+        throw std::runtime_error(get_last_error());
+    }
+
+
+    //bind
+    void socket::bind(const socket_address& address) {
+        int error = ::bind(m_handle, reinterpret_cast<const sockaddr*>(address.data()), socket_address::data_size);
+
+        if (!error) {
+            return;
+        }
+
+        throw std::runtime_error(get_last_error());
+    }
+
+
+    //connect
+    void socket::connect(const socket_address& address) {
+        int error = ::connect(m_handle, reinterpret_cast<const sockaddr*>(address.data()), socket_address::data_size);
+
+        if (!error) {
+            return;
+        }
+
+        throw std::runtime_error(get_last_error());
+    }
+
+
+    //send
+    size_t socket::send(const void* buffer, size_t size, int flags) {
+        check_buffer_size(size);
+        int bytes_sent = ::send(m_handle, reinterpret_cast<const char*>(buffer), static_cast<int>(size), flags);
+        return handle_send_result(m_handle, bytes_sent);
+    }
+
+
+    //send-to
+    size_t socket::send(const void* buffer, size_t size, const socket_address& addr, int flags) {
+        check_buffer_size(size);
+        int bytes_sent = ::sendto(m_handle, reinterpret_cast<const char*>(buffer), static_cast<int>(size), flags, reinterpret_cast<const sockaddr*>(addr.data()), socket_address::data_size);
+        return handle_send_result(m_handle, bytes_sent);
+    }
+
+
+    //receive
+    size_t socket::receive(void* buffer, size_t size, int flags) {
+        check_buffer_size(size);
+        int bytes_received = recv(m_handle, reinterpret_cast<char*>(buffer), static_cast<int>(size), flags);
+        return handle_receive_result(m_handle, bytes_received);
+    }
+
+
+    //receive from
+    size_t socket::receive(void* buffer, size_t size, socket_address& addr, int flags) {
+        check_buffer_size(size);
+        int bytes_received = recvfrom(m_handle, reinterpret_cast<char*>(buffer), static_cast<int>(size), flags, reinterpret_cast<sockaddr*>(addr.data()), nullptr);
+        return handle_receive_result(m_handle, bytes_received);
+    }
+
+
+    //send packet
+    size_t socket::send_packet(const void* buffer, size_t size, int flags) {
+        size_t remaining_size = size;
+
+        while (remaining_size > 0) {
+            size_t bytes_sent = send(buffer, remaining_size, flags);
+
+            if (!bytes_sent) {
+                return 0;
+            }
+
+            remaining_size -= bytes_sent;
+            buffer = reinterpret_cast<const char*>(buffer) + bytes_sent;
+        }
+
+        return size;
+    }
+
+
+    //send packet to
+    size_t socket::send_packet(const void* buffer, size_t size, const socket_address& addr, int flags) {
+        size_t remaining_size = size;
+
+        while (remaining_size > 0) {
+            size_t bytes_sent = send(buffer, remaining_size, addr, flags);
+
+            if (!bytes_sent) {
+                return 0;
+            }
+
+            remaining_size -= bytes_sent;
+            buffer = reinterpret_cast<const char*>(buffer) + bytes_sent;
+        }
+
+        return size;
+    }
+
+
+    //receive packet
+    size_t socket::receive_packet(void* buffer, size_t size, int flags) {
+        size_t remaining_size = size;
+
+        while (remaining_size > 0) {
+            size_t bytes_received = receive(buffer, remaining_size, flags);
+
+            if (!bytes_received) {
+                return 0;
+            }
+
+            remaining_size -= bytes_received;
+            buffer = reinterpret_cast<char*>(buffer) + bytes_received;
+        }
+
+        return size;
+    }
+
+
+    //receive packet from
+    size_t socket::receive_packet(void* buffer, size_t size, socket_address& addr, int flags) {
+        size_t remaining_size = size;
+
+        while (remaining_size > 0) {
+            size_t bytes_received = receive(buffer, remaining_size, addr, flags);
+
+            if (!bytes_received) {
+                return 0;
+            }
+
+            remaining_size -= bytes_received;
+            buffer = reinterpret_cast<char*>(buffer) + bytes_received;
+        }
+
+        return size;
+    }
+
+
+} //namespace netlib
